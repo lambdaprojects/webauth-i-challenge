ec8fc9f8d0cfbbf1da8273912f6c6029831b8dff
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-10 16:33:44 -0500

    Created a middle ware and routes for auth and user

--- api/server.js
+++ api/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const authRouter = require("../auth/auth-router.js");
+const userRouter = require("../users/user-router.js");
 
 const server = express();
 server.use(express.json());
@@ -7,4 +9,6 @@ server.get("/", (req, res) => {
   res.send(`<h3>You have reached the default page of API Server</h3>`);
 });
 
+server.use("/api/auth", authRouter);
+server.use("/api/users", userRouter);
 module.exports = server;
---
+++ auth/auth-router.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+
+const UserHelper = require("../users/user-model.js");
+
+router.post("/register", async (req, res) => {
+  try {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash;
+
+    let addUser = await UserHelper.add(user);
+    res.status(201).json(addUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  UserHelper.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+module.exports = router;
